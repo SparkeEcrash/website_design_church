fb0769c1f67ea6cdd020c912eac9aa15563e8d53
SparkeEcrash <jpark121989@gmail.com>
2019-09-09 04:12:48 -0700

    add initial home page

--- public/dist/main.js
+++ public/dist/main.js
@@ -1,1 +1,13 @@
-console.log("june");
+console.log('june');
+
+$(document).ready(function() {
+	$(window).scroll(function() {
+		let position = $(this).scrollTop();
+
+		if (position > 50) {
+			$(".navigation").addClass("navigation-toggle");
+		} else {
+			$(".navigation").removeClass("navigation-toggle");
+		}
+	})
+})
